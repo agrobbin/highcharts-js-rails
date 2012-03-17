1082065981ee328bb9a62a39077341d07eb458d4
Alex Robbin <alex@robbinsweb.biz>
2012-03-17 19:55:51 -0400

    take into account the fact that sometimes the data passed to a series may be an array of arrays

--- lib/highcharts/series.rb
+++ lib/highcharts/series.rb
@@ -2,7 +2,7 @@ class Highcharts
   class Series < Base
 
     def to_json
-      @options[:data] = options[:data].collect(&:to_f)
+      @options[:data] = options[:data].first.is_a?(Array) ? options[:data].collect {|d| [d.first, d.last.to_f]} : options[:data].collect(&:to_f)
       super
     end
 
